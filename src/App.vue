c117451e324dd4b6aa0f7cad408605460453d7fa
Thomas Ferro <t.ferro184@gmail.com>
2018-01-17 21:26:19 +0100

    Vue + Vuetify init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      fixed
+      app
+      v-model="drawer"
+    >
+      <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Good Accounts
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
+import App from './App';
+// import routes from './routes';
+import '../node_modules/vuetify/dist/vuetify.min.css';
+
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+const router = new VueRouter({
+  mode: 'history',
+  // routes,
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+});
